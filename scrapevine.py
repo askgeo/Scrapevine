3a9af87f2cab3b633d3ab850e494d404dd7a96cf
Abigail Knapp <abigail.s.knapp@gmail.com>
2021-11-23 16:54:32 -0500

    Cleaned up unused code chunks

--- scrapevine.py
+++ scrapevine.py
@@ -1,5 +1,6 @@
-# Scrapevine, a First Webscraper Project
-# Project basis and code snippets from realpython tutorial at https://realpython.com/python-web-scraping-practical-introduction
+# scrapevine, a First Webscraper Project
+# Project basis and code snippets from realpython tutorial
+# at https://realpython.com/python-web-scraping-practical-introduction
 # Abigail Knapp 11/2021
 
 from urllib.request import urlopen
@@ -11,17 +12,7 @@ page = urlopen(url)
 html_bytes = page.read()
 html = html_bytes.decode("utf-8")
 
-# Print webpage html
-#print(html)
-
-# Using regex, pull out just the html with title tags, accounting for mistyped html
-#pattern = "<title.*?>.*?</title.*?>"
-#match_results = re.search(pattern, html, re.IGNORECASE)
-#title = match_results.group()
-#title = re.sub("<.*?>", "", title) # Removes HTML tags
-#print(title)
-
-# Find favorite animal and color
+# Find favorite animal using regex
 animal_pattern = "animal:.*?<*?br*?>"
 animal_match_results = re.search(animal_pattern, html, re.IGNORECASE)
 animal1 = animal_match_results.group()
@@ -29,17 +20,11 @@ animal1 = animal_match_results.group()
 animal2 = re.sub("animal:", "", animal1) # Removes search term wrappers #1
 #print(animal2)
 animal_result = re.sub("<*?br*?>", "", animal2) # Removes search term wrappers #2
-print(animal_result)
+animal_clean = animal_result.strip()
+print(animal_clean)
 
-# Find favorite animal and color
-name_pattern = "Name:.*?<*?br*?>"
-name_match_results = re.search(name_pattern, html, re.IGNORECASE)
-name1 = name_match_results.group()
-name2 = re.sub("Name:", "", name1) # Removes search term wrappers #1
-name_result = re.sub("<*?br*?>", "", name2) # Removes search term wrappers #2
-print(name_result)
+# Find name and favorite color using a loop
 
-print("or...")
 
 to_find = ["Name:", "Favorite Color:"]
 for string in ["Name:", "Favorite Color:"]:
@@ -47,7 +32,9 @@ for string in ["Name:", "Favorite Color:"]:
     text_start_idx = string_start_idx + len(string)
 
     next_tag_idx = html[text_start_idx:].find("<")
-    text_end_idx = text__start_idx + next_tag_idx
-    raw_text = html[text_idx:string_end_idx]
-    clean_text = raw_text.strip(\r)
+    text_end_idx = text_start_idx + next_tag_idx
+    raw_text = html[text_start_idx:text_end_idx]
+    clean_text = raw_text.strip()
+    assert isinstance(clean_text, object)
     print(clean_text)
+
